d5799857ec2d0d95aab0c30c9a30ea0dfef03f7f
Zhao Guangjian <zgj0315@gmail.com>
2017-03-22 18:18:13 +0800

    add submit cmd

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 sbt -java-home /Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home package
+
+# spark-submit --master yarn-cluster --class org.after90.spark.WordCount sbtprojecttest_2.10-0.1.0.jar
