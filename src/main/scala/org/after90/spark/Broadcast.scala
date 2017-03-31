d7cb28994660df4bba185cc5e564a0a1cce4f76f
Zhao Guangjian <zgj0315@gmail.com>
2017-03-31 11:14:56 +0800

    some test

--- src/main/scala/org/after90/spark/Broadcast.scala
+++ src/main/scala/org/after90/spark/Broadcast.scala
@@ -4,15 +4,15 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by zhaogj on 30/03/2017.
-  * spark2.0里初始化sc已经不是这么玩了，这里只是做个纪念罢了
   */
-object InitializingSpark {
+object Broadcast {
   def main(args: Array[String]): Unit = {
     val strAppName = "strAppName"
     val strMaster = "local[*]"
     val conf = new SparkConf().setAppName(strAppName).setMaster(strMaster)
     val sc = new SparkContext(conf)
-
+    val broadcastVar = sc.broadcast(Array(1, 2, 3))
+    println(broadcastVar.value)
     sc.stop()
   }
 }
