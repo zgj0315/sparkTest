fa23e892a8e4fce3c04275a1f93d8495ab62ea6e
Zhao Guangjian <zgj0315@gmail.com>
2017-05-19 15:03:13 +0800

    some change

--- src/main/scala/org/after90/spark/YOBSpark.scala
+++ src/main/scala/org/after90/spark/YOBSpark.scala
@@ -1,6 +1,7 @@
 package org.after90.spark
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 /**
   * Created by zhaogj on 31/03/2017.
@@ -21,25 +22,37 @@ object YOBSpark {
     val sc = spark.sparkContext
 
     val res = sc.wholeTextFiles("./data/names/*.txt", 40)
-      //将
       .flatMap { case (strFilePath, strContent) => {
-      val nFilePathLength = strFilePath.length
-      val strYear = strFilePath.substring(nFilePathLength - 8, nFilePathLength - 4)
-      val astrLine = strContent.split("\n")
-      val nstrLineSize = astrLine.size
-      for (i <- 0 until nstrLineSize) {
-        astrLine(i) = strYear + ";" + astrLine(i)
-      }
-      astrLine
-    }
-    }
-      .map { x => {
-        val astrPart = x.split(";")
-        (astrPart(0), 1)
+        val nFilePathLength = strFilePath.length
+        val strYear = strFilePath.substring(nFilePathLength - 8, nFilePathLength - 4)
+        val astrLine = strContent.split("\n")
+        val nstrLineSize = astrLine.size
+        for (i <- 0 until nstrLineSize) {
+          astrLine(i) = strYear + "," + astrLine(i)
+        }
+        astrLine
       }
       }
-      .reduceByKey(_ + _)
-      .foreach(println)
+    // The schema is encoded in a string
+    val strSchema = "birth name sex count"
+    // Generate the schema based on the string of schema
+    val fields = strSchema.split(" ")
+      .map(fieldName => StructField(fieldName, StringType, nullable = true))
+    val schema = StructType(fields)
+
+    val rowRDD = res.map(x => {
+      val astrPart = x.split(",")
+      Row(astrPart(0), astrPart(1), astrPart(2), astrPart(3))
+    })
+
+    val nameDF = spark.createDataFrame(rowRDD, schema)
+
+    nameDF.createOrReplaceTempView("yob")
+
+    //    spark.sql("SELECT birth, SUM(count) FROM yob GROUP BY birth ORDER BY birth").show()
+    //    spark.sql("SELECT sex, SUM(count) FROM yob GROUP BY sex ORDER BY sex").show()
+    //spark.sql("SELECT sex, name, SUM(count) FROM yob GROUP BY sex, name ORDER BY sum(CAST(count AS DOUBLE)) DESC").show()
+    spark.sql("SELECT SUM(count) FROM yob").show()
 
     spark.stop()
   }
