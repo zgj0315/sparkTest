f0f79294c16017e79b927fffda32b68e76d4c4a7
Zhao Guangjian <zgj0315@gmail.com>
2017-12-31 13:03:11 +0800

    reangeBetween can work

--- src/main/scala/org/after90/spark/sql/EventSafeMaker.scala
+++ src/main/scala/org/after90/spark/sql/EventSafeMaker.scala
@@ -11,22 +11,25 @@ object EventSafeMaker {
       .getOrCreate()
     import spark.implicits._
     val df = List(
-      ("012229", "2017-01-01"),
-      ("012229", "2017-01-02"),
-      ("012229", "2017-01-03"),
-      ("012229", "2017-01-04"),
-      ("012230", "2017-01-01"),
-      ("012230", "2017-01-02"),
-      ("012231", "2017-01-01")
-    ).toDF("udid", "date")
+      ("012229", 1, 1),
+      ("012229", 2, 2),
+      ("012229", 3, 1),
+      ("012229", 4, 1),
+      ("012229", 8, 1),
+      ("012229", 9, 1),
+      ("012229", 10, 1),
+      ("012230", 1, 1),
+      ("012230", 2, 1)
+    ).toDF("udid", "time", "count")
     import org.apache.spark.sql.expressions.Window
     import org.apache.spark.sql.functions._
     val wSpec = Window.partitionBy("udid")
-      .orderBy("date")
-      .rowsBetween(-1, 1)//三天
+      .orderBy("time")
+      //.rowsBetween(-1, 1)
+      .rangeBetween(-1, 1) //三天
 
     df.withColumn("movingCount",
-      count(df("*")).over(wSpec)).show()
+      sum(df("count")).over(wSpec)).show()
 
     spark.close()
 
