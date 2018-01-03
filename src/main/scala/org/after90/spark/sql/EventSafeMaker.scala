3634ab5137c015651bc788754931d21df451c9a2
Zhao Guangjian <zgj0315@gmail.com>
2018-01-03 15:49:45 +0800

    change data

--- src/main/scala/org/after90/spark/sql/EventSafeMaker.scala
+++ src/main/scala/org/after90/spark/sql/EventSafeMaker.scala
@@ -11,26 +11,27 @@ object EventSafeMaker {
       .getOrCreate()
     import spark.implicits._
     val df = List(
-      ("012229", 1, 1),
-      ("012229", 2, 2),
-      ("012229", 3, 1),
-      ("012229", 4, 1),
-      ("012229", 8, 1),
-      ("012229", 9, 1),
-      ("012229", 10, 1),
-      ("012230", 1, 1),
-      ("012230", 2, 1)
+      ("012229", 1234567811, 1),
+      ("012229", 1234567812, 2),
+      ("012229", 1234567813, 1),
+      ("012229", 1234567815, 1),
+      ("012229", 1234567818, 1),
+      ("012229", 1234567819, 1),
+      ("012229", 1234567820, 1),
+      ("012230", 1234567891, 1),
+      ("012230", 1234567892, 1)
     ).toDF("udid", "time", "count")
     import org.apache.spark.sql.expressions.Window
     import org.apache.spark.sql.functions._
     val wSpec = Window.partitionBy("udid")
       .orderBy("time")
       //.rowsBetween(-1, 1)
-      .rangeBetween(-1, 1) //三天
+      .rangeBetween(-2, 0) //三秒
 
     df.withColumn("movingCount",
       sum(df("count")).over(wSpec)).show()
 
+    Thread.sleep(1000 * 60 * 60)
     spark.close()
 
   }
